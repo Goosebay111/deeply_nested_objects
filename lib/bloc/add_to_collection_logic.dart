a752e974b84dbc24f5645d97dc337d5166124dc6
George Lee <george.p.lee.iii@gmail.com>
2022-09-22 00:27:27 +1000

    save

--- lib/bloc/add_to_collection_logic.dart
+++ lib/bloc/add_to_collection_logic.dart
@@ -1,40 +1,30 @@
-import 'dart:math';
-
 import 'package:deeply_nested_objects/bloc/collection_bloc.dart';
 import 'package:deeply_nested_objects/bloc/collection_event.dart';
 import 'package:deeply_nested_objects/bloc/collection_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void addToCollectionLogic(ShowType showType, int index, int count, BuildContext context) {
-  if (showType == ShowType.collection) {
-    BlocProvider.of<CollectionBloc>(context).add(AddInfo(
-      child: CollectionState(
-        name: "Series $count",
-        showType: ShowType.series,
-        children: [],
-      ),
-      index: index,
-    ));
-  }
-  if (showType == ShowType.series) {
-    BlocProvider.of<CollectionBloc>(context).add(AddInfo(
-      child: CollectionState(
-        name: 'Season $count',
-        showType: ShowType.season,
-        children: [],
-      ),
-      index: index,
-    ));
-  }
-  if (showType == ShowType.season) {
-    BlocProvider.of<CollectionBloc>(context).add(AddInfo(
-      child: CollectionState(
-        name: "Episode $count",
-        showType: ShowType.episode,
-        children: [],
-      ),
-      index: index,
-    ));
+void addToCollectionLogic(
+    ShowType showType, int index, int count, BuildContext context) {
+  void passToBloc(String name, ShowType showType) =>
+      BlocProvider.of<CollectionBloc>(context).add(
+        AddInfo(
+          index: index,
+          child: CollectionState(name: name, showType: showType, children: []),
+        ),
+      );
+
+  switch (showType) {
+    case ShowType.collection:
+      passToBloc('Series $count', ShowType.series);
+      break;
+    case ShowType.series:
+      passToBloc('Season $count', ShowType.season);
+      break;
+    case ShowType.season:
+      passToBloc('Episode $count', ShowType.episode);
+      break;
+    case ShowType.episode:
+      break;
   }
 }
