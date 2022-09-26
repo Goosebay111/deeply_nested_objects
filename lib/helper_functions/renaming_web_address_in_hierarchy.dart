18ae35cb2395ddaaf81631b036a57cbd5cdefd4e
George Lee <george.p.lee.iii@gmail.com>
2022-09-27 00:24:47 +1000

    save

--- lib/helper_functions/renaming_web_address_in_hierarchy.dart
+++ lib/helper_functions/renaming_web_address_in_hierarchy.dart
@@ -4,10 +4,10 @@ import 'package:deeply_nested_objects/bloc/collection/collection_state.dart';
 CollectionState renameWebAddressInHierarchy(
     CollectionState state, UpdateNodeWebAddress event) {
   return state.copyWith(
-      webAddress:
-          state == event.parent ? event.newWebAddress : state.webAddress,
-      children: [
-        for (var child in state.children)
-          renameWebAddressInHierarchy(child, event)
-      ]);
+    webAddress: state == event.parent ? event.newWebAddress : state.webAddress,
+    children: [
+      for (var child in state.children)
+        renameWebAddressInHierarchy(child, event)
+    ],
+  );
 }
