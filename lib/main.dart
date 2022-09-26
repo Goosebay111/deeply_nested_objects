418ef955fb3a65a4cd783418c678f9ab64265c61
George Lee <george.p.lee.iii@gmail.com>
2022-09-26 18:57:19 +1000

    save

--- lib/main.dart
+++ lib/main.dart
@@ -3,30 +3,43 @@ import 'package:deeply_nested_objects/pages/home_page.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
-main() => runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final storage = await HydratedStorage.build(
+    storageDirectory: await getApplicationDocumentsDirectory(),
+  );
+  HydratedBlocOverrides.runZoned(
+    () => runApp(RepositoryProvider(
+      create: (context) => CollectionBloc(),
+      child: const MyApp(),
+    )),
+    storage: storage,
+  );
+}
+
+//runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => CollectionBloc(),
-      child: MaterialApp(
-        title: 'ToVo App',
-        theme: ThemeData(
-          brightness: Brightness.light,
-          colorScheme: const ColorScheme.light(background: Colors.white),
-          primaryColor: Colors.white,
-          appBarTheme: const AppBarTheme(
-            backgroundColor: Colors.white,
-            titleTextStyle:
-                TextStyle(color: Colors.black87, fontWeight: FontWeight.w700),
-          ),
+    return MaterialApp(
+      title: 'ToVo App',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        colorScheme: const ColorScheme.light(background: Colors.white),
+        primaryColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          titleTextStyle:
+              TextStyle(color: Colors.black87, fontWeight: FontWeight.w700),
         ),
-        home: const MyHomePage(),
       ),
+      home: const MyHomePage(),
     );
   }
 }
