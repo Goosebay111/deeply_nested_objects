48abf3fcee9221cc7329b0fb525abebe70fc2312
George Lee <george.p.lee.iii@gmail.com>
2022-09-28 08:56:00 +1000

    save

--- lib/bloc/collection/collection_bloc.dart
+++ lib/bloc/collection/collection_bloc.dart
@@ -6,12 +6,13 @@ import 'package:deeply_nested_objects/helper_functions/delete_node_from_hierarch
 import 'package:deeply_nested_objects/helper_functions/renaming_node_in_hierarchy.dart';
 import 'package:deeply_nested_objects/helper_functions/renaming_web_address_in_hierarchy.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
+
 // saved for good work
 class CollectionBloc extends HydratedBloc<CollectionEvents, CollectionState> {
   CollectionBloc() : super(CollectionState.initial()) {
-    ///
     on<AddToTopLayerData>(
-        (event, emit) => emit(addToTopLayerData(state, event)));
+      (event, emit) => emit(addToTopLayerData(state, event)),
+    );
 
     on<AddToDeeplyNestedData>(
       (event, emit) {
